02294357ca936227e7281d502e5a8797f24c6f6e
zhuyuhan2333 <zhuyuhan2333@qq.com>
2024-09-20 22:05:44 +0800

    feat: support multi-center with attention network

--- model_aggregate.py
+++ model_aggregate.py
@@ -16,7 +16,7 @@ from graph import EdgeType, HeteroWithGraphIndex, NodeType
 from typing import Dict, Set
 import sys
 import copy
-from itertools import combinations
+from model_attention import AttentionLayer
 from Config import RnnType
 
 
@@ -107,9 +107,9 @@ class HeteroGlobalAttentionPooling(nn.Module):
         graph = to_homogeneous(h_graph)
         with graph.local_scope():
             gate = self.gate_nn(feat_all)
-            assert (
-                    gate.shape[-1] == 1
-            ), "The output of gate_nn should have size 1 at the last axis."
+            # assert (
+            #         gate.shape[-1] == 1
+            # ), "The output of gate_nn should have size 1 at the last axis."
             feat = self.feat_nn(feat_all) if self.feat_nn else feat_all
 
             graph.ndata["gate"] = gate
@@ -122,6 +122,7 @@ class HeteroGlobalAttentionPooling(nn.Module):
             time_series_size = gate.shape[1]
             if get_attention:
                 attention_scores = torch.max(input=gate, dim=1)[0]
+                # attention_scores = gate
                 return readout, index, time_series_size, attention_scores
             else:
                 return readout, index, time_series_size
@@ -132,6 +133,7 @@ class AggrHGraphConvWindows(nn.Module):
                  rnn: RnnType = RnnType.LSTM):
         super(AggrHGraphConvWindows, self).__init__()
         self.hidden_size = hidden_channel
+        self.out_size = out_channel
         if rnn == RnnType.LSTM:
             self.rnn_layer = nn.LSTM(input_size=self.hidden_size, hidden_size=self.hidden_size, num_layers=2,
                                      batch_first=True)
@@ -143,7 +145,10 @@ class AggrHGraphConvWindows(nn.Module):
         self.linear = nn.Linear(self.hidden_size, 1)
         self.output_layer = nn.Softmax(dim=0)
         self.activation = nn.ReLU()
-        self.pooling = HeteroGlobalAttentionPooling(gate_nn=nn.Linear(self.hidden_size, out_channel))
+        # self.pooling = HeteroGlobalAttentionPooling(gate_nn=nn.Linear(self.hidden_size, out_channel))
+        self.pooling = HeteroGlobalAttentionPooling(gate_nn=nn.Linear(self.hidden_size, hidden_channel))
+        self.center_attention = AttentionLayer(hidden_channel, out_channel, num_heads=1)
+        self.node_attention = AttentionLayer(hidden_channel, out_channel, num_heads=1)
 
     def forward(self, graphs: Dict[str, HeteroWithGraphIndex]):
         output_data_list = []
@@ -164,7 +169,6 @@ class AggrHGraphConvWindows(nn.Module):
             window_anomaly_time_series.append(graph.anomaly_time_series)
             output_data_list.append(output_feat)
             window_graphs_index.append(index)
-            atten_sorted.append(attention_scores)
             # convert to the index of the current graph
             graph_center_node_index = {}
             for center in graphs_center_node_name:
@@ -202,6 +206,24 @@ class AggrHGraphConvWindows(nn.Module):
                             graphs_anomaly_node_index[anomaly]['neighbor'][center] = neighbors_type
                         else:
                             graphs_anomaly_node_index[anomaly]['source'] = [index[graph_anomaly_node]]
+            # Apply center attention
+            attention_scores_after_center = th.zeros([attention_scores.shape[0], self.out_size])
+            center_embeddings = []
+            for center in graph_center_node_index:
+                center_nodes_index = []
+                for _, nodes_index in graph_center_node_index[center].items():
+                    center_nodes_index.extend(nodes_index)
+                aggr_center = th.mean(attention_scores[sorted(center_nodes_index)], dim=0, keepdim=True)
+                center_embeddings.append(aggr_center)
+            center_embeddings = th.cat(center_embeddings, dim=0)
+            aggr_feat_weighted, attention_weights_center = self.center_attention(center_embeddings, center_embeddings,
+                                                                                 center_embeddings)
+            for i, center in enumerate(graph_center_node_index):
+                center_nodes_index = []
+                for _, nodes_index in graph_center_node_index[center].items():
+                    center_nodes_index.extend(nodes_index)
+                attention_scores_after_center[center_nodes_index] = th.max(attention_scores[sorted(center_nodes_index)] * aggr_feat_weighted[i], dim=1)[0].unsqueeze(-1)
+            atten_sorted.append(attention_scores_after_center)
             window_graphs_center_node_index.append(graph_center_node_index)
             window_graphs_anomaly_node_index.append(graphs_anomaly_node_index)
         output = self.activation(self.linear(self.rnn_layer(th.stack(output_data_list, dim=0))[0]))
@@ -219,8 +241,8 @@ class AggrUnsupervisedGNN(nn.Module):
         self.conv = AggrHGraphConvWindows(out_channel=out_channels, hidden_channel=hidden_size,
                                           svc_feat_num=svc_feat_num, instance_feat_num=instance_feat_num,
                                           node_feat_num=node_feat_num, rnn=rnn)
-        self.precessor_neighbor_center_weight = nn.Parameter(
-            th.ones(len(anomaly_index), len(list(center_map.keys())), requires_grad=True, device='cpu'))
+        # self.precessor_neighbor_center_weight = nn.Parameter(
+        #     th.ones(len(anomaly_index), len(list(center_map.keys())), requires_grad=True, device='cpu'))
         anomaly_index_reverse = {idx: an for an, idx in anomaly_index.items()}
         anomaly_nodes_maps = [sorted_graph.anomaly_name for sorted_graph in sorted_graphs]
         self.graphs_anomaly_center_nodes = []
@@ -281,23 +303,23 @@ class AggrUnsupervisedGNN(nn.Module):
     def loss(self, aggr_feat, aggr_center_index, aggr_anomaly_index, window_graphs_index, window_time_series_sizes,
              window_anomaly_time_series):
         aggr_index_combine_list = []
-        for aggr in aggr_center_index:
-            anomaly_index_combine = {}
-            for center in aggr:
-                for node_type in aggr[center]:
-                    if center not in anomaly_index_combine:
-                        anomaly_index_combine[center] = aggr[center][node_type]
-                    else:
-                        anomaly_index_combine[center].extend(aggr[center][node_type])
-            aggr_index_combine_list.append(anomaly_index_combine)
+        # for aggr in aggr_center_index:
+        #     anomaly_index_combine = {}
+        #     for center in aggr:
+        #         for node_type in aggr[center]:
+        #             if center not in anomaly_index_combine:
+        #                 anomaly_index_combine[center] = aggr[center][node_type]
+        #             else:
+        #                 anomaly_index_combine[center].extend(aggr[center][node_type])
+        #     aggr_index_combine_list.append(anomaly_index_combine)
         sum_criterion = 0
-        for idx, anomaly_index_combine in enumerate(aggr_index_combine_list):
-            aggr_feat_idx = aggr_feat[idx]
-            for center in anomaly_index_combine:
-                aggr_center = aggr_feat_idx[sorted(anomaly_index_combine[center])]
-                aggr_mean = th.mean(aggr_center).item()
-                mean = torch.full_like(aggr_center, aggr_mean)
-                sum_criterion += self.criterion(aggr_center, mean)
+        # for idx, anomaly_index_combine in enumerate(aggr_index_combine_list):
+        #     aggr_feat_idx = aggr_feat[idx]
+        #     for center in anomaly_index_combine:
+        #         aggr_center = aggr_feat_idx[sorted(anomaly_index_combine[center])]
+        #         aggr_mean = th.mean(aggr_center).item()
+        #         mean = torch.full_like(aggr_center, aggr_mean)
+        #         sum_criterion += self.criterion(aggr_center, mean)
 
         for idx, anomaly_index_combine in enumerate(aggr_anomaly_index):
             aggr_feat_idx = aggr_feat[idx]
@@ -314,8 +336,9 @@ class AggrUnsupervisedGNN(nn.Module):
                         for center in aggr_anomaly_nodes_index['neighbor']:
                             for ano_idx_idx, ano_idx in enumerate(aggr_anomaly_nodes_index['neighbor'][center]):
                                 center_node_weight = anomaly_graph_anomaly_center_nodes_weight[center]
-                                precessor_rate = 1 * self.precessor_neighbor_center_weight[self.anomaly_index[anomaly]][
-                                    self.center_map[center]] * center_node_weight()[ano_idx_idx]
+                                # precessor_rate = 1 * self.precessor_neighbor_center_weight[self.anomaly_index[anomaly]][
+                                #     self.center_map[center]] * center_node_weight()[ano_idx_idx]
+                                precessor_rate = 1 * center_node_weight()[ano_idx_idx]
                                 neighbor_index_matrix = torch.tensor(
                                     aggr_anomaly_nodes_index['neighbor'][center][ano_idx_idx])
                                 aggr_feat_label_weight[neighbor_index_matrix] = precessor_rate
